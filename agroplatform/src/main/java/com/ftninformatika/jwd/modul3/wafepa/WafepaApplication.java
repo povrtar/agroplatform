b9cc9b1a9419e3f1cb7e6d56a5290345158c026d
povrtar <43764769+povrtar@users.noreply.github.com>
2021-03-24 18:30:27 +0100

    Update WafepaApplication.java

--- agroplatform/src/main/java/com/ftninformatika/jwd/modul3/wafepa/WafepaApplication.java
+++ agroplatform/src/main/java/com/ftninformatika/jwd/modul3/wafepa/WafepaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class WafepaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		 SpringApplication.run(WafepaApplication.class, args);
 	}
 
